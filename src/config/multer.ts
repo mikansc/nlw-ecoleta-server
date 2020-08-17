4df7ab1ce314d3bac17b38625532e3c9fe503fee
Michael Nascimento <michael_thewebmaster@iMac-de-Michael.local>
2020-08-17 20:53:43 -0300

    Configured AWS Img upload

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,16 +1,56 @@
-import multer from 'multer';
-import path from 'path';
-import crypto from 'crypto';
+import "dotenv/config";
 
-export default {
-  storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'uploads'),
+import multer from "multer";
+import multerS3 from "multer-s3";
+import path from "path";
+import crypto from "crypto";
+import aws from "aws-sdk";
+
+const storageTypes = {
+  local: multer.diskStorage({
+    destination: path.resolve(__dirname, "..", "..", "temp"),
     filename: (req, file, callback) => {
-      const hash = crypto.randomBytes(6).toString('hex');
-
+      const hash = crypto.randomBytes(6).toString("hex");
+      file.key = `${hash}-${file.originalname}`;
+      callback(null, file.key);
+    },
+  }),
+  s3: multerS3({
+    s3: new aws.S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    }),
+    bucket: "mn-ecoleta",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: "public-read",
+    key: (req, file, callback) => {
+      const hash = crypto.randomBytes(6).toString("hex");
       const fileName = `${hash}-${file.originalname}`;
-
       callback(null, fileName);
     },
   }),
+};
+
+export default {
+  dest: path.resolve(__dirname, "..", "..", "temp"),
+  storage: storageTypes["s3"],
+  // storage: multer.diskStorage({
+  //   destination: path.resolve(__dirname, "..", "..", "temp"),
+  //   filename: (req, file, callback) => {
+  //     const hash = crypto.randomBytes(6).toString("hex");
+  //     const fileName = `${hash}-${file.originalname}`;
+  //     callback(null, fileName);
+  //   },
+  // }),
+  // limits: {
+  //   fileSize: 2 * 1024 * 1024,
+  // },
+  // fileFilter: (req, file, callback) => {
+  //   const allowedMimes = ["image/jpeg", "image/png"];
+  //   if (allowedMimes.includes(file.mimetype)) {
+  //     callback(null, true);
+  //   } else {
+  //     callback(new Error("Invalid file type."));
+  //   }
+  // },
 };
--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -1,45 +1,47 @@
-import { Request, Response } from 'express';
-import knex from '../database/connection';
+import { Request, Response } from "express";
+import knex from "../database/connection";
 
 class PointsController {
   async index(req: Request, res: Response) {
     try {
       //cidade, uf, items (Query Params)
-    const { city, uf, items } = req.query;
+      const { city, uf, items } = req.query;
 
-    const parsedItems = String(items)
-      .split(',')
-      .map((item) => Number(item.trim()));
+      const parsedItems = String(items)
+        .split(",")
+        .map((item) => Number(item.trim()));
 
-    const points = await knex('points')
-      .join('point_items', 'points.id', '=', 'point_items.point_id')
-      .whereIn('point_items.item_id', parsedItems)
-      .where('city', String(city))
-      .where('uf', String(uf))
-      .distinct()
-      .select('points.*');
+      const points = await knex("points")
+        .join("point_items", "points.id", "=", "point_items.point_id")
+        .whereIn("point_items.item_id", parsedItems)
+        .where("city", String(city))
+        .where("uf", String(uf))
+        .distinct()
+        .select("points.*");
 
-    const serializedPoints = points.map((point) => {
-      return {
-        ...point,
-        image_url: `http://192.168.15.4:5500/uploads/${point.image}`,
-      };
-    });
+      const serializedPoints = points.map((point) => {
+        return {
+          ...point,
+          image_url: `http://192.168.15.4:5500/uploads/${point.image}`,
+        };
+      });
 
-    return res.json(serializedPoints);
+      return res.json(serializedPoints);
     } catch (error) {
-      console.error(error.message)
-      return res.status(400).send({error: "An error has ocurred on trying to get all points."})
+      console.error(error.message);
+      return res
+        .status(400)
+        .send({ error: "An error has ocurred on trying to get all points." });
     }
   }
 
   async show(req: Request, res: Response) {
     const { id } = req.params;
 
-    const point = await knex('points').where('id', id).first();
+    const point = await knex("points").where("id", id).first();
 
     if (!point) {
-      return res.status(400).json({ message: 'Point Not Found' });
+      return res.status(400).json({ message: "Point Not Found" });
     }
 
     const serializedPoint = {
@@ -47,16 +49,15 @@ class PointsController {
       image_url: `http://192.168.15.4:5500/uploads/${point.image}`,
     };
 
-    const items = await knex('items')
-      .join('point_items', 'items.id', '=', 'point_items.item_id')
-      .where('point_items.point_id', id)
-      .select('items.title');
+    const items = await knex("items")
+      .join("point_items", "items.id", "=", "point_items.item_id")
+      .where("point_items.point_id", id)
+      .select("items.title");
 
     return res.json({ point: serializedPoint, items });
   }
 
   async create(req: Request, res: Response) {
-
     try {
       const {
         name,
@@ -68,11 +69,12 @@ class PointsController {
         uf,
         items,
       } = req.body;
-  
+
       const trx = await knex.transaction();
-  
+      console.log(req.file);
+      const { key } = req.file;
       const point = {
-        image: req.file.filename,
+        image: key,
         name,
         email,
         whatsapp,
@@ -81,13 +83,14 @@ class PointsController {
         city,
         uf,
       };
-  
-      const insertedIds = await trx('points').insert(point).returning('id');
-  
+
+      console.log(point);
+      const insertedIds = await trx("points").insert(point).returning("id");
+
       const point_id = insertedIds[0];
-  
+
       const pointItems = items
-        .split(',')
+        .split(",")
         .map((item: string) => Number(item.trim()))
         .map((item_id: number) => {
           return {
@@ -95,17 +98,20 @@ class PointsController {
             point_id,
           };
         });
-      await trx('point_items').insert(pointItems);
-  
+      await trx("point_items").insert(pointItems);
+
       await trx.commit();
-  
+
       return res.json({
         id: point_id,
         ...point,
+        image: `https://mn-ecoleta.s3.us-east-2.amazonaws.com/${point.image}`,
       });
     } catch (error) {
-      console.error(error.message)
-      return res.status(400).send({error: "An error has ocurred on trying to create the point."})
+      console.error(error.message);
+      return res
+        .status(400)
+        .send({ error: "An error has ocurred on trying to create the point." });
     }
   }
 }
